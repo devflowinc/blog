cfe5f8a2f443b1e28da693dbaa4ab6c5211e5a64
Nicholas Khami <nicholas.khami@gmail.com>
2023-05-14 17:44:34 -0500

    feature: changed blog to inkpaper theme

--- apps/frontend/src/content/config.ts
+++ apps/frontend/src/content/config.ts
@@ -1,21 +1,8 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from "astro:content";
+import { blogSchema } from "./_schemas";
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
+  schema: blogSchema,
 });
 
 export const collections = { blog };
